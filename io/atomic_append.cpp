01fd6fded5c724068fcf1ce95827780c3671862c
孙煜晗 <SENSETIME\sunyuhan@cn0314000677l.domain.sensetime.com>
2022-12-27 19:29:59 +0800

    homework 5-3 compare open(fd, O_APPEND | O_WRONLY) with open(fd, O_WRONLY) + lseek(fd, 0, SEEK_END)

--- io/atomic_append.cpp
+++ io/atomic_append.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
     if(argc < 3) {
@@ -8,6 +10,15 @@ int main(int argc, char *argv[]) {
     }
     char *filename = argv[1];
     int len = atoi(argv[2]);
-    int fd = open(filename, O_APPEND | O_WRONLY);
-    
+    int fd;
+    if(argc == 4 && std::string(argv[3]) == "x") {
+        fd = open(filename, O_WRONLY);
+        lseek(fd, 0, SEEK_END);
+    } else {
+        fd = open(filename, O_APPEND | O_WRONLY);
+    }
+    std::cout << fd << " " << strerror(errno) << std::endl;
+    char *buf = (char*)malloc(len);
+    memset(buf, 0, len); 
+    write(fd, buf, len);
 }
